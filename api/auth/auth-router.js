b9c797fd0883982bee8dc1856ed47513090c252f
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-15 23:23:21 -0800

    login endpoint busted. tomorrow~

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const router = require("express").Router();
 const tokenBuilder = require("./token-builder");
 
+
 const Users = require("../users/users-model.js");
 
 // router.post("/register", (req, res, next) => {
@@ -20,24 +21,37 @@ const Users = require("../users/users-model.js");
 // });
 
 router.post("/register", (req, res, next) => {
-    const { username, password, email, role_id } = req.body;
-    if (!username || !password || !email || !role_id) {
-      //create middleware for this you lazy butt
-      //assign role_id to be guest upon no entry
-      next({ status: 400, message: "username, password, email and role required" });
-    }{
-      Users.add(req.body) // add encryption later
-        .then((newUser) => {
-          res.json({ message: `Welcome ${newUser}` });
-        })
-        .catch(next);
-    }
-  });
+    let user = req.body;
 
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(user.password, rounds);
 
-
-router.post("/login", (req, res, next) => {
-  res.json("logging in... syke");
+    user.password = hash;
+    console.log(user.password)
+    Users.add(user) // add encryption later
+    .then((newUser) => {
+        res.status(201).json({ message: `Welcome ${newUser}` });
+    })
+    .catch(next);
 });
+
+
+
+// router.post('/login', (req, res, next) => {
+//     let { username, password } = req.body;
+//     // middleware??? 
+//     Users.findByUsername({ username })
+//       .then(([user]) => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           const token = tokenBuilder(user)
+//           res.status(200).json({
+//             message: `Welcome back ${user.username}!`, token
+//           });
+//         } else {
+//           next({ status: 401, message: 'Invalid Credentials' });
+//         }
+//       })
+//       .catch(next);
+//   });
 
 module.exports = router;
