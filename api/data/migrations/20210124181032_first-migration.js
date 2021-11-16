d5629cf94d3962eac919d85d9b9eeac3f97a26ae
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-15 16:11:34 -0800

    events and foods table added

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -4,10 +4,10 @@ exports.up = async (knex) => {
       roles.increments('role_id')
       roles.string('role_name', 32).notNullable().unique()
     })
-    .createTable('users', (users) => {
+    .createTable('users', users => {
       users.increments('user_id')
-      users.string('username', 200).notNullable().unique()
-      users.string('password', 200).notNullable()
+      users.string('username', 64).notNullable().unique()
+      users.string('password', 64).notNullable()
       users.string('email', 128).notNullable()
       users.integer('role_id')
            .unsigned()
@@ -17,10 +17,32 @@ exports.up = async (knex) => {
           //  .onUpdate('RESTRICT') ERROR: foreign_key constraint - why?
           //  .onDelete('RESTRICT')
     })
+    .createTable('events', events => {
+      events.increments('event_id')
+      events.string('event_name', 128).notNullable()
+      events.string('location', 128).notNullable()
+      events.string('date', 128).notNullable()
+      events.string('time', 128).notNullable()
+      events.string('event_description', 256)
+      events.integer('attendees')
+      .unsigned()
+      .notNullable()
+      .references('user_id')
+      .inTable('users')
+          //  .onUpdate('RESTRICT')
+          //  .onDelete('RESTRICT')
+    })
+    .createTable('foods', meals => {
+      meals.increments('food_id')
+      meals.string('food_name', 128).notNullable()
+      meals.string('food_description', 256)
+    })
 }
 
 exports.down = async (knex) => {
   await knex.schema
     .dropTableIfExists('users')
     .dropTableIfExists('roles')
+    .dropTableIfExists('events')
+    .dropTableIfExists('foods')
 }
